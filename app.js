a119b20ff5be34ecc71f360eaa8385b06046c4c3
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-06-27 19:42:29 +0500

    new files

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+let a=0
+// function right() {
+//    a=a+30
+//    var left=document.getElementById('player1');
+//    left.style.marginLeft=a+"px";
+ 
+// }
+// function left() {
+  
+//    var left=document.getElementById('player1');
+//    left.style.marginLeft=0+"px";
+ 
+// }
+function right() {
+   a=a+30
+   var left=document.getElementById('player2');
+   left.style.marginRight=a+"px";
+ 
+}
+function left() {
+  
+   var left=document.getElementById('player2');
+   left.style.marginRight=-30+"px";
+ 
+}
+document.getElementById('player1')ad
+//setInterval(right,1000);
